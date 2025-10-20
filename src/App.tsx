b0059b42c69fbb7518c7e9e55b4d4df106c983c2
Omphile Hope Hlongwane <ST10026037@vcconnect.edu.za>
2025-10-20 13:06:36 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Omphile Hlongwane</h1>
+      <img
+        src="/Omphile Hlongwane.jpg"
+        alt="Omphile Hlongwane"
+        style={{ borderRadius: '50%', marginTop: '20px' }}
+      />
+      <p style={{ marginTop: '20px' }}>
+        Welcome to my React + TypeScript + SWC app deployed on Netlify!
+      </p>
+    </div>
+  );
+}
+
+export default App;
